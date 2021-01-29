bba8b9fee55d41b7ee68b19176320c9350b44182
Huzaifah_Adams <Conanbbc1999@gmail.com>
2021-01-29 11:33:16 +0300

    a robofriend web app without redux

---
+++ README.md
@@ -0,0 +1,5 @@
+a simple robo friend web app  v0.1.0
+how to use:
+1. npm install / getting the packages for the app
+2. npm start/ starts the server
+...done... server running 
---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+
+class ErrorBoundary extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = { hasError: false };
+    }
+  
+    static getDerivedStateFromError(error) {
+      // Update state so the next render will show the fallback UI.
+      return { hasError: true };
+    }
+  
+    componentDidCatch(error, errorInfo) {
+        this.setState({hasError: true})
+    }
+  
+    render() {
+      if (this.state.hasError) {
+        // You can render any custom fallback UI
+        return <h1>Something went wrong.</h1>;
+      }
+  
+      return this.props.children; 
+    }
+  }
+  export default ErrorBoundary;
